5633ff3e232cfbff4be3a31678b7c42bf9dc7ce2
Krish28060606 <krish.dubey_cs.aiml24@gla.ac.in>
2026-04-30 11:28:55 +0530

    Refactor get_connection to use DATABASE_URL

--- db.py
+++ db.py
@@ -1,11 +1,5 @@
+import os
 import psycopg2
 
 def get_connection():
-    conn = psycopg2.connect(
-        host="localhost",
-        database="ai_creative",
-        user="postgres",
-        password="282006",   # change if different
-        port="5432"
-    )
-    return conn
+    return psycopg2.connect(os.environ.get("DATABASE_URL"))
