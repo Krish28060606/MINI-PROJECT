f5f5056f33302a026aba21e57cc4f49b913c64f9
Krish28060606 <krish.dubey_cs.aiml24@gla.ac.in>
2026-04-30 12:24:44 +0530

    Update db.py

--- db.py
+++ db.py
@@ -2,4 +2,4 @@ import os
 import psycopg2
 
 def get_connection():
-    return psycopg2.connect(os.environ.get("DATABASE_URL"))
+    return psycopg2.connect(os.environ.get("DATABASE_URL"), sslmode="require")
